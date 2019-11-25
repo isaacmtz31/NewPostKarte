f9347a10ecefaff7f9b3703a36b9e6d489091316
isaacmtz31 <isaac_31120@outlook.com>
2019-11-25 15:30:40 -0600

    Explorar final

--- js/explorar.js
+++ js/explorar.js
@@ -3,13 +3,13 @@ var globalData = "";
 $(document).ready(function() {
     $("#busqueda a").click(function() {
         var val = $(this).data('value') // would be 5
-        //swal("Ok!", "Buscando imagenes de la categoria ".concat(val) , "success");
+        swal("Ok!", "Buscando imagenes de la categoria ".concat(val) , "success");
         $.ajax({
             type: "POST",
             dataType: "json",
             data: {"test" : val},
             url: './../pages/explorarPersonalizada_AX.php',
-            success: function(data) {
+            success: function(data) {              
               globalData = data;
               swal("Genial", "Nuevos recursos encontrados", "success");
               document.getElementById("yy").innerHTML = "";
--- pages/Explorar_Imgs_BD.php
+++ pages/Explorar_Imgs_BD.php
@@ -5,13 +5,13 @@
     $categorias="";
     while ($row = mysqli_fetch_array($resInfUser, MYSQLI_BOTH))
     {
-      $categorias .= "<li id='busqueda'>" . "<a href=# data-value='" . $row["Categorias"]. "'>" . "<img src=" . $row["imagen"] . " alt='dumy'><h3>" . $row["Categorias"] . "</h3></a></li>";
+      $categorias .= "<li id='busqueda'>" . "<a href='#' data-value='" . $row["Categorias"]. "'>" . "<img src=" . $row["imagen"] . " alt='dumy'><h3>" . $row["Categorias"] . "</h3></a></li>";
     }
     $papeles="";
     $sqlInfPapeles = "select * from explorarPapeles";
     $resInfUser = mysqli_query($conexion, $sqlInfPapeles);
     while ($row = mysqli_fetch_array($resInfUser, MYSQLI_BOTH))
     {
-      $papeles .= "<li><a href=#><img src=" . $row["img"] . " alt='dumy'><h3>" . $row["Papeles"] . "</h3></a></li>";
+      $papeles .= "<li><a href=><img src=" . $row["img"] . " alt='dumy'><h3>" . $row["Papeles"] . "</h3></a></li>";
     }
  ?>
